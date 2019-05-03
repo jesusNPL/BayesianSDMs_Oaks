58d26f53dfe4c03429ec0a1ee415f479cb0be0cc
Jesús N. Pinto-Ledezma <jesuspintoledezma@gmail.com>
2019-05-03 11:11:29 -0500

    Update saveBinaries.R

--- saveBinaries.R
+++ saveBinaries.R
@@ -1,22 +1,5 @@
-
-
-###Load layers (change pattern if not using ascii)###
-layers <- list.files(path = "", pattern = 'asc', full.names = TRUE)
-###Set working directory###
-setwd("/Users/lewis_jones/Desktop/Practice")
-###run for loop for binary conversion###
-for (i in layers) { # look for each ascii file in layers
-  binary<-raster(i)
-  ###set desired threshold###
-  binary[binary < 0.296927723] <- 0
-  binary[binary > 0] <- 1
-  plot(binary)
-  name <- i
-  writeRaster(binary, filename=name, format="ascii", overwrite=TRUE)
-}
-dev.off()
-
 setwd("Ensembles/RS")
+
 library(raster)
 lf <- list.files(pattern = "tif$")
 quercus <- stack(lf)
@@ -30,6 +13,7 @@ writeBinary <- function(sppStack, nSP, thr = 0.1){
     spp <- sppStack[[sp]]
     spp[spp < thr] <- 0
     spp[spp > 0] <- 1
+    plot(spp)
     writeRaster(spp, filename = paste("Binaries/", spnames[sp], sep = ""), format = "GTiff", overwrite = TRUE)
   }
 }

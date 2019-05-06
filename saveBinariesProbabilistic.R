a79e2bb15d39dcf682382bfd8510f62dce51b1e3
Jesús N. Pinto-Ledezma <jesuspintoledezma@gmail.com>
2019-05-06 14:06:12 -0500

    Update saveBinariesProbabilistic.R

--- saveBinariesProbabilistic.R
+++ saveBinariesProbabilistic.R
@@ -20,18 +20,25 @@ writeBinary2 <- function(pamOBS, sppStack, spNames, cover = 0.9){
 # for loop to assing species predictions and write predictions based on probabilities of occurrence
   for(sp in 1:length(spNames)){
     print(spNames[sp])
+    # Separate individual species to establish cutoffs
     occ <- pamOBS[spNames[sp]]
     occ <- cbind(pamOBS[, 1:2], occ)
     occ <- occ[occ[, 3] == 1, ]
-    #names(occ) <- c("x", "y", spNames[sp])
-    
+    # Make cutoffs
     cutOFF <- ecospat.mpa(sppStack[[sp]], occ[, 1:2], perc = cover)
+    # reclassify continuous raster to a binary map using cutoffs
     sppBinary <- ecospat.binary.model(sppStack[[sp]], cutOFF)
     plot(sppBinary)
+    # Save the binary map
     writeRaster(sppBinary, filename = paste("Binaries2/", spNames[sp], sep = ""), format = "GTiff", overwrite = TRUE)
     print("Binary predictions completed ! Check your results in the Binaries directory.")
   }
 }
+
+# Usage:
+# 1. Load species occurrences, this must be an incidence matrix. This is the same incidence matrix that is used to make the species ensmbles.
+# 2. A raster stack of N species
+# 3. Coverage "cover" or percentage of occurrences used for establish the cutoffs. 
 
 writeBinary2(pamOBS = OCC, sppStack = quercus, spNames = sppnames, cover = 1)
 

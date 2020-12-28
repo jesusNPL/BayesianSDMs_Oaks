a44f40ea8908f8f355bd76b7f87f1916331d656f
Jesús N. Pinto-Ledezma <jesuspintoledezma@gmail.com>
2020-12-28 17:22:02 -0600

    Add files via upload
    adding function to estimate richness metrics

---
+++ Auxiliary/SR_metrics.R
@@ -0,0 +1,13 @@
+SRmetrics <- function(predSR, obsSR) { 
+  
+  SRdev <- abs(obsSR - predSR)/max(obsSR)
+  SRdiff <- (obsSR - predSR)
+  SR_change <- ((predSR - obsSR)/obsSR)
+  # negative SR_change values suggest that community underprediction
+  # positive SR_change values suggest that community overprediction
+  RRL <- log(predSR/obsSR)
+  results <- data.frame(SR_obs = obsSR, SR_pred = predSR,  
+                        Deviation = SRdev, Difference = SRdiff, Change = SR_change, LRR = RRL)
+  return(results)
+}
+

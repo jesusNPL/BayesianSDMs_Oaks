b6c8a67faf270456fb19f35dff207824b130700a
Jesús N. Pinto-Ledezma <jesuspintoledezma@gmail.com>
2020-12-28 17:22:59 -0600

    Update SR_metrics.R

--- Auxiliary/SR_metrics.R
+++ Auxiliary/SR_metrics.R
@@ -3,8 +3,8 @@ SRmetrics <- function(predSR, obsSR) {
   SRdev <- abs(obsSR - predSR)/max(obsSR)
   SRdiff <- (obsSR - predSR)
   SR_change <- ((predSR - obsSR)/obsSR)
-  # negative SR_change values suggest that community underprediction
-  # positive SR_change values suggest that community overprediction
+  # negative SR_change values suggest SR community underprediction
+  # positive SR_change values suggest SR community overprediction
   RRL <- log(predSR/obsSR)
   results <- data.frame(SR_obs = obsSR, SR_pred = predSR,  
                         Deviation = SRdev, Difference = SRdiff, Change = SR_change, LRR = RRL)
